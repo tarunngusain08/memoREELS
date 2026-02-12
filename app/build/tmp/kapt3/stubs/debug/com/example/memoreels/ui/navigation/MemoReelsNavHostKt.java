6800f44a327f50c0df4c9e7b5d90a8ddc9d6f9c2
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-02-13 05:24:26 +0530

    building the memoREELS app: MemoReelsNavHostKt.java

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/memoreels/ui/navigation/MemoReelsNavHostKt.java
@@ -0,0 +1,31 @@
+package com.example.memoreels.ui.navigation;
+
+import androidx.compose.animation.EnterTransition;
+import androidx.compose.animation.ExitTransition;
+import androidx.compose.material.icons.Icons;
+import androidx.compose.runtime.Composable;
+import androidx.compose.ui.Modifier;
+import androidx.compose.ui.graphics.vector.ImageVector;
+import androidx.navigation.NavHostController;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000$\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a\u0012\u0010\n\u001a\u00020\u000b2\b\b\u0002\u0010\f\u001a\u00020\rH\u0007\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0082T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0004\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0005\u001a\u00020\u0006X\u0082\u0004\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0007\u001a\u00020\u0006X\u0082\u0004\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\b\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\t\u001a\u00020\u0006X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u000e"}, d2 = {"ANIM_DURATION", "", "slideInFromLeft", "Landroidx/compose/animation/EnterTransition;", "slideInFromRight", "slideOutToLeft", "Landroidx/compose/animation/ExitTransition;", "slideOutToRight", "tabEnter", "tabExit", "MemoReelsNavHost", "", "navController", "Landroidx/navigation/NavHostController;", "app_debug"})
+public final class MemoReelsNavHostKt {
+    private static final int ANIM_DURATION = 300;
+    @org.jetbrains.annotations.NotNull()
+    private static final androidx.compose.animation.EnterTransition tabEnter = null;
+    @org.jetbrains.annotations.NotNull()
+    private static final androidx.compose.animation.ExitTransition tabExit = null;
+    @org.jetbrains.annotations.NotNull()
+    private static final androidx.compose.animation.EnterTransition slideInFromRight = null;
+    @org.jetbrains.annotations.NotNull()
+    private static final androidx.compose.animation.ExitTransition slideOutToRight = null;
+    @org.jetbrains.annotations.NotNull()
+    private static final androidx.compose.animation.EnterTransition slideInFromLeft = null;
+    @org.jetbrains.annotations.NotNull()
+    private static final androidx.compose.animation.ExitTransition slideOutToLeft = null;
+    
+    @androidx.compose.runtime.Composable()
+    public static final void MemoReelsNavHost(@org.jetbrains.annotations.NotNull()
+    androidx.navigation.NavHostController navController) {
+    }
+}
