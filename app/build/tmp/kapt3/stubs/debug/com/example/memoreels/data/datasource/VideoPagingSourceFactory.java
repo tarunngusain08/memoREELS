3006d6ba6217ce7b04065001b28b33034a9445ab
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-02-13 05:24:21 +0530

    building the memoREELS app: VideoPagingSourceFactory.java

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/memoreels/data/datasource/VideoPagingSourceFactory.java
@@ -0,0 +1,24 @@
+package com.example.memoreels.data.datasource;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@javax.inject.Singleton()
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000*\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010 \n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0010\u000b\n\u0000\b\u0007\u0018\u00002\u00020\u0001B\u000f\b\u0007\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J*\u0010\u0005\u001a\u00020\u00062\u000e\b\u0002\u0010\u0007\u001a\b\u0012\u0004\u0012\u00020\t0\b2\b\b\u0002\u0010\n\u001a\u00020\t2\b\b\u0002\u0010\u000b\u001a\u00020\fR\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\r"}, d2 = {"Lcom/example/memoreels/data/datasource/VideoPagingSourceFactory;", "", "context", "Landroid/content/Context;", "(Landroid/content/Context;)V", "create", "Lcom/example/memoreels/data/datasource/VideoPagingSource;", "exclusions", "", "", "query", "shuffle", "", "app_debug"})
+public final class VideoPagingSourceFactory {
+    @org.jetbrains.annotations.NotNull()
+    private final android.content.Context context = null;
+    
+    @javax.inject.Inject()
+    public VideoPagingSourceFactory(@org.jetbrains.annotations.NotNull()
+    android.content.Context context) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.example.memoreels.data.datasource.VideoPagingSource create(@org.jetbrains.annotations.NotNull()
+    java.util.List<java.lang.String> exclusions, @org.jetbrains.annotations.NotNull()
+    java.lang.String query, boolean shuffle) {
+        return null;
+    }
+}
