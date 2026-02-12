2dfedfbe7da45aed1fe4e53e9a4c2b1ddf6e6978
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-02-13 05:24:26 +0530

    building the memoREELS app: Screen.java

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/memoreels/ui/navigation/Screen.java
@@ -0,0 +1,168 @@
+package com.example.memoreels.ui.navigation;
+
+import androidx.compose.animation.EnterTransition;
+import androidx.compose.animation.ExitTransition;
+import androidx.compose.material.icons.Icons;
+import androidx.compose.runtime.Composable;
+import androidx.compose.ui.Modifier;
+import androidx.compose.ui.graphics.vector.ImageVector;
+import androidx.navigation.NavHostController;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u00000\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u000b\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\b6\u0018\u00002\u00020\u0001:\u0005\r\u000e\u000f\u0010\u0011B\u001f\b\u0004\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0003\u0012\u0006\u0010\u0005\u001a\u00020\u0006\u00a2\u0006\u0002\u0010\u0007R\u0011\u0010\u0005\u001a\u00020\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\b\u0010\tR\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\n\u0010\u000bR\u0011\u0010\u0004\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\f\u0010\u000b\u0082\u0001\u0005\u0012\u0013\u0014\u0015\u0016\u00a8\u0006\u0017"}, d2 = {"Lcom/example/memoreels/ui/navigation/Screen;", "", "route", "", "title", "icon", "Landroidx/compose/ui/graphics/vector/ImageVector;", "(Ljava/lang/String;Ljava/lang/String;Landroidx/compose/ui/graphics/vector/ImageVector;)V", "getIcon", "()Landroidx/compose/ui/graphics/vector/ImageVector;", "getRoute", "()Ljava/lang/String;", "getTitle", "Explore", "Favorites", "Feed", "Onboarding", "Player", "Lcom/example/memoreels/ui/navigation/Screen$Explore;", "Lcom/example/memoreels/ui/navigation/Screen$Favorites;", "Lcom/example/memoreels/ui/navigation/Screen$Feed;", "Lcom/example/memoreels/ui/navigation/Screen$Onboarding;", "Lcom/example/memoreels/ui/navigation/Screen$Player;", "app_debug"})
+public abstract class Screen {
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String route = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String title = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.compose.ui.graphics.vector.ImageVector icon = null;
+    
+    private Screen(java.lang.String route, java.lang.String title, androidx.compose.ui.graphics.vector.ImageVector icon) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getRoute() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getTitle() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.compose.ui.graphics.vector.ImageVector getIcon() {
+        return null;
+    }
+    
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000$\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0000\b\u00c6\n\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u0013\u0010\u0003\u001a\u00020\u00042\b\u0010\u0005\u001a\u0004\u0018\u00010\u0006H\u00d6\u0003J\t\u0010\u0007\u001a\u00020\bH\u00d6\u0001J\t\u0010\t\u001a\u00020\nH\u00d6\u0001\u00a8\u0006\u000b"}, d2 = {"Lcom/example/memoreels/ui/navigation/Screen$Explore;", "Lcom/example/memoreels/ui/navigation/Screen;", "()V", "equals", "", "other", "", "hashCode", "", "toString", "", "app_debug"})
+    public static final class Explore extends com.example.memoreels.ui.navigation.Screen {
+        @org.jetbrains.annotations.NotNull()
+        public static final com.example.memoreels.ui.navigation.Screen.Explore INSTANCE = null;
+        
+        private Explore() {
+        }
+        
+        @java.lang.Override()
+        public boolean equals(@org.jetbrains.annotations.Nullable()
+        java.lang.Object other) {
+            return false;
+        }
+        
+        @java.lang.Override()
+        public int hashCode() {
+            return 0;
+        }
+        
+        @java.lang.Override()
+        @org.jetbrains.annotations.NotNull()
+        public java.lang.String toString() {
+            return null;
+        }
+    }
+    
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000$\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0000\b\u00c6\n\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u0013\u0010\u0003\u001a\u00020\u00042\b\u0010\u0005\u001a\u0004\u0018\u00010\u0006H\u00d6\u0003J\t\u0010\u0007\u001a\u00020\bH\u00d6\u0001J\t\u0010\t\u001a\u00020\nH\u00d6\u0001\u00a8\u0006\u000b"}, d2 = {"Lcom/example/memoreels/ui/navigation/Screen$Favorites;", "Lcom/example/memoreels/ui/navigation/Screen;", "()V", "equals", "", "other", "", "hashCode", "", "toString", "", "app_debug"})
+    public static final class Favorites extends com.example.memoreels.ui.navigation.Screen {
+        @org.jetbrains.annotations.NotNull()
+        public static final com.example.memoreels.ui.navigation.Screen.Favorites INSTANCE = null;
+        
+        private Favorites() {
+        }
+        
+        @java.lang.Override()
+        public boolean equals(@org.jetbrains.annotations.Nullable()
+        java.lang.Object other) {
+            return false;
+        }
+        
+        @java.lang.Override()
+        public int hashCode() {
+            return 0;
+        }
+        
+        @java.lang.Override()
+        @org.jetbrains.annotations.NotNull()
+        public java.lang.String toString() {
+            return null;
+        }
+    }
+    
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000$\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0000\b\u00c6\n\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u0013\u0010\u0003\u001a\u00020\u00042\b\u0010\u0005\u001a\u0004\u0018\u00010\u0006H\u00d6\u0003J\t\u0010\u0007\u001a\u00020\bH\u00d6\u0001J\t\u0010\t\u001a\u00020\nH\u00d6\u0001\u00a8\u0006\u000b"}, d2 = {"Lcom/example/memoreels/ui/navigation/Screen$Feed;", "Lcom/example/memoreels/ui/navigation/Screen;", "()V", "equals", "", "other", "", "hashCode", "", "toString", "", "app_debug"})
+    public static final class Feed extends com.example.memoreels.ui.navigation.Screen {
+        @org.jetbrains.annotations.NotNull()
+        public static final com.example.memoreels.ui.navigation.Screen.Feed INSTANCE = null;
+        
+        private Feed() {
+        }
+        
+        @java.lang.Override()
+        public boolean equals(@org.jetbrains.annotations.Nullable()
+        java.lang.Object other) {
+            return false;
+        }
+        
+        @java.lang.Override()
+        public int hashCode() {
+            return 0;
+        }
+        
+        @java.lang.Override()
+        @org.jetbrains.annotations.NotNull()
+        public java.lang.String toString() {
+            return null;
+        }
+    }
+    
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000$\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0000\b\u00c6\n\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u0013\u0010\u0003\u001a\u00020\u00042\b\u0010\u0005\u001a\u0004\u0018\u00010\u0006H\u00d6\u0003J\t\u0010\u0007\u001a\u00020\bH\u00d6\u0001J\t\u0010\t\u001a\u00020\nH\u00d6\u0001\u00a8\u0006\u000b"}, d2 = {"Lcom/example/memoreels/ui/navigation/Screen$Onboarding;", "Lcom/example/memoreels/ui/navigation/Screen;", "()V", "equals", "", "other", "", "hashCode", "", "toString", "", "app_debug"})
+    public static final class Onboarding extends com.example.memoreels.ui.navigation.Screen {
+        @org.jetbrains.annotations.NotNull()
+        public static final com.example.memoreels.ui.navigation.Screen.Onboarding INSTANCE = null;
+        
+        private Onboarding() {
+        }
+        
+        @java.lang.Override()
+        public boolean equals(@org.jetbrains.annotations.Nullable()
+        java.lang.Object other) {
+            return false;
+        }
+        
+        @java.lang.Override()
+        public int hashCode() {
+            return 0;
+        }
+        
+        @java.lang.Override()
+        @org.jetbrains.annotations.NotNull()
+        public java.lang.String toString() {
+            return null;
+        }
+    }
+    
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000$\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0000\b\u00c6\n\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u0013\u0010\u0003\u001a\u00020\u00042\b\u0010\u0005\u001a\u0004\u0018\u00010\u0006H\u00d6\u0003J\t\u0010\u0007\u001a\u00020\bH\u00d6\u0001J\t\u0010\t\u001a\u00020\nH\u00d6\u0001\u00a8\u0006\u000b"}, d2 = {"Lcom/example/memoreels/ui/navigation/Screen$Player;", "Lcom/example/memoreels/ui/navigation/Screen;", "()V", "equals", "", "other", "", "hashCode", "", "toString", "", "app_debug"})
+    public static final class Player extends com.example.memoreels.ui.navigation.Screen {
+        @org.jetbrains.annotations.NotNull()
+        public static final com.example.memoreels.ui.navigation.Screen.Player INSTANCE = null;
+        
+        private Player() {
+        }
+        
+        @java.lang.Override()
+        public boolean equals(@org.jetbrains.annotations.Nullable()
+        java.lang.Object other) {
+            return false;
+        }
+        
+        @java.lang.Override()
+        public int hashCode() {
+            return 0;
+        }
+        
+        @java.lang.Override()
+        @org.jetbrains.annotations.NotNull()
+        public java.lang.String toString() {
+            return null;
+        }
+    }
+}
