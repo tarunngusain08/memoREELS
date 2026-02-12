5aea55f95fd962f41d5121b4cee106d0ea0234ff
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-02-13 05:24:25 +0530

    building the memoREELS app: ToggleFavoriteUseCase.java

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/memoreels/domain/usecase/ToggleFavoriteUseCase.java
@@ -0,0 +1,24 @@
+package com.example.memoreels.domain.usecase;
+
+import com.example.memoreels.domain.model.Video;
+import com.example.memoreels.domain.repository.VideoRepository;
+import javax.inject.Inject;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000b\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u000f\b\u0007\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u0016\u0010\u0005\u001a\u00020\u00062\u0006\u0010\u0007\u001a\u00020\bH\u0086B\u00a2\u0006\u0002\u0010\tR\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\n"}, d2 = {"Lcom/example/memoreels/domain/usecase/ToggleFavoriteUseCase;", "", "repository", "Lcom/example/memoreels/domain/repository/VideoRepository;", "(Lcom/example/memoreels/domain/repository/VideoRepository;)V", "invoke", "", "video", "Lcom/example/memoreels/domain/model/Video;", "(Lcom/example/memoreels/domain/model/Video;Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "app_debug"})
+public final class ToggleFavoriteUseCase {
+    @org.jetbrains.annotations.NotNull()
+    private final com.example.memoreels.domain.repository.VideoRepository repository = null;
+    
+    @javax.inject.Inject()
+    public ToggleFavoriteUseCase(@org.jetbrains.annotations.NotNull()
+    com.example.memoreels.domain.repository.VideoRepository repository) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.Object invoke(@org.jetbrains.annotations.NotNull()
+    com.example.memoreels.domain.model.Video video, @org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super java.lang.Boolean> $completion) {
+        return null;
+    }
+}
