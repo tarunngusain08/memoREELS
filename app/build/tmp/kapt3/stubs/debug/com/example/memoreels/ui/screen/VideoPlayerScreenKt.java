20861991258bbb085f17439fb86f56df3efca932
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-02-13 05:24:28 +0530

    building the memoREELS app: VideoPlayerScreenKt.java

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/memoreels/ui/screen/VideoPlayerScreenKt.java
@@ -0,0 +1,20 @@
+package com.example.memoreels.ui.screen;
+
+import android.net.Uri;
+import androidx.compose.material.icons.Icons;
+import androidx.compose.runtime.Composable;
+import androidx.compose.ui.Alignment;
+import androidx.compose.ui.Modifier;
+import androidx.media3.common.MediaItem;
+import androidx.media3.common.Player;
+import androidx.media3.exoplayer.ExoPlayer;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u0014\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a\u001e\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\f\u0010\u0004\u001a\b\u0012\u0004\u0012\u00020\u00010\u0005H\u0007\u00a8\u0006\u0006"}, d2 = {"VideoPlayerScreen", "", "videoUri", "", "onBack", "Lkotlin/Function0;", "app_debug"})
+public final class VideoPlayerScreenKt {
+    
+    @androidx.compose.runtime.Composable()
+    public static final void VideoPlayerScreen(@org.jetbrains.annotations.NotNull()
+    java.lang.String videoUri, @org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function0<kotlin.Unit> onBack) {
+    }
+}
