dc379c6feed348dc1a0b0bd9dc436dd44324b459
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-02-13 05:24:25 +0530

    building the memoREELS app: MetadataOverlayKt.java

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/memoreels/ui/components/MetadataOverlayKt.java
@@ -0,0 +1,18 @@
+package com.example.memoreels.ui.components;
+
+import androidx.compose.runtime.Composable;
+import androidx.compose.ui.Alignment;
+import androidx.compose.ui.Modifier;
+import androidx.compose.ui.graphics.Brush;
+import androidx.compose.ui.text.style.TextOverflow;
+import com.example.memoreels.domain.model.Video;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u001a\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000b\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a\"\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u00052\b\b\u0002\u0010\u0006\u001a\u00020\u0007H\u0007\u00a8\u0006\b"}, d2 = {"MetadataOverlay", "", "video", "Lcom/example/memoreels/domain/model/Video;", "visible", "", "modifier", "Landroidx/compose/ui/Modifier;", "app_debug"})
+public final class MetadataOverlayKt {
+    
+    @androidx.compose.runtime.Composable()
+    public static final void MetadataOverlay(@org.jetbrains.annotations.NotNull()
+    com.example.memoreels.domain.model.Video video, boolean visible, @org.jetbrains.annotations.NotNull()
+    androidx.compose.ui.Modifier modifier) {
+    }
+}
