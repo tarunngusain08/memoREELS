b7ad01fc5f13adb89abb518904d70a72313905fe
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-02-13 05:24:25 +0530

    building the memoREELS app: FavoriteButtonKt.java

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/memoreels/ui/components/FavoriteButtonKt.java
@@ -0,0 +1,20 @@
+package com.example.memoreels.ui.components;
+
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.os.VibratorManager;
+import androidx.compose.material.icons.Icons;
+import androidx.compose.runtime.Composable;
+import androidx.compose.ui.Alignment;
+import androidx.compose.ui.Modifier;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u001a\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u000b\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a(\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\f\u0010\u0004\u001a\b\u0012\u0004\u0012\u00020\u00010\u00052\b\b\u0002\u0010\u0006\u001a\u00020\u0007H\u0007\u00a8\u0006\b"}, d2 = {"FavoriteButton", "", "isFavorite", "", "onToggle", "Lkotlin/Function0;", "modifier", "Landroidx/compose/ui/Modifier;", "app_debug"})
+public final class FavoriteButtonKt {
+    
+    @androidx.compose.runtime.Composable()
+    public static final void FavoriteButton(boolean isFavorite, @org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function0<kotlin.Unit> onToggle, @org.jetbrains.annotations.NotNull()
+    androidx.compose.ui.Modifier modifier) {
+    }
+}
