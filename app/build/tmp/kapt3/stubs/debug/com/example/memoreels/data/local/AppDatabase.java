f0409cff27cf88f73944224e1f3496c95b11d87e
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-02-13 05:24:21 +0530

    building the memoREELS app: AppDatabase.java

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/memoreels/data/local/AppDatabase.java
@@ -0,0 +1,21 @@
+package com.example.memoreels.data.local;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+import com.example.memoreels.data.model.FavoriteEntity;
+import com.example.memoreels.data.model.VideoTagEntity;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u0018\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\b'\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0003\u001a\u00020\u0004H&J\b\u0010\u0005\u001a\u00020\u0006H&\u00a8\u0006\u0007"}, d2 = {"Lcom/example/memoreels/data/local/AppDatabase;", "Landroidx/room/RoomDatabase;", "()V", "favoritesDao", "Lcom/example/memoreels/data/local/FavoritesDao;", "videoTagDao", "Lcom/example/memoreels/data/local/VideoTagDao;", "app_debug"})
+@androidx.room.Database(entities = {com.example.memoreels.data.model.FavoriteEntity.class, com.example.memoreels.data.model.VideoTagEntity.class}, version = 2, exportSchema = false)
+public abstract class AppDatabase extends androidx.room.RoomDatabase {
+    
+    public AppDatabase() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public abstract com.example.memoreels.data.local.FavoritesDao favoritesDao();
+    
+    @org.jetbrains.annotations.NotNull()
+    public abstract com.example.memoreels.data.local.VideoTagDao videoTagDao();
+}
