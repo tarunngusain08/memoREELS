c2ace23ca1baf40d05339dc3444d3d4209be514b
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-02-13 05:22:52 +0530

    building the memoREELS app: _com_example_memoreels_MemoReelsApplication.java

---
+++ app/build/generated/hilt/component_sources/debug/dagger/hilt/internal/aggregatedroot/codegen/_com_example_memoreels_MemoReelsApplication.java
@@ -0,0 +1,21 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "com.example.memoreels.MemoReelsApplication",
+    rootPackage = "com.example.memoreels",
+    originatingRoot = "com.example.memoreels.MemoReelsApplication",
+    originatingRootPackage = "com.example.memoreels",
+    rootAnnotation = HiltAndroidApp.class,
+    rootSimpleNames = "MemoReelsApplication",
+    originatingRootSimpleNames = "MemoReelsApplication"
+)
+@Generated("dagger.hilt.processor.internal.root.AggregatedRootGenerator")
+public class _com_example_memoreels_MemoReelsApplication {
+}
