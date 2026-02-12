5eff83c50ced5599e76d2c4442894f27f94ab648
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-02-13 05:24:27 +0530

    building the memoREELS app: OnboardingScreenKt.java

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/memoreels/ui/screen/OnboardingScreenKt.java
@@ -0,0 +1,21 @@
+package com.example.memoreels.ui.screen;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import androidx.compose.foundation.layout.Arrangement;
+import androidx.compose.runtime.Composable;
+import androidx.compose.ui.Alignment;
+import androidx.compose.ui.Modifier;
+import androidx.compose.ui.text.style.TextAlign;
+import com.google.accompanist.permissions.ExperimentalPermissionsApi;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u000e\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a\u0016\u0010\u0000\u001a\u00020\u00012\f\u0010\u0002\u001a\b\u0012\u0004\u0012\u00020\u00010\u0003H\u0007\u00a8\u0006\u0004"}, d2 = {"OnboardingScreen", "", "onComplete", "Lkotlin/Function0;", "app_debug"})
+public final class OnboardingScreenKt {
+    
+    @kotlin.OptIn(markerClass = {com.google.accompanist.permissions.ExperimentalPermissionsApi.class})
+    @androidx.compose.runtime.Composable()
+    public static final void OnboardingScreen(@org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function0<kotlin.Unit> onComplete) {
+    }
+}
