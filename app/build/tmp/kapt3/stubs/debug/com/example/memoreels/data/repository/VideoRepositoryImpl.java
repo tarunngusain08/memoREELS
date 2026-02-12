6915c419db121ac59ca7d4c7bd90e6ee029f09ad
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-02-13 05:24:23 +0530

    building the memoREELS app: VideoRepositoryImpl.java

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/memoreels/data/repository/VideoRepositoryImpl.java
@@ -0,0 +1,76 @@
+package com.example.memoreels.data.repository;
+
+import androidx.paging.Pager;
+import androidx.paging.PagingConfig;
+import androidx.paging.PagingData;
+import com.example.memoreels.data.datasource.VideoPagingSourceFactory;
+import com.example.memoreels.data.local.FavoritesDao;
+import com.example.memoreels.data.model.FavoriteEntity;
+import com.example.memoreels.data.model.VideoEntity;
+import com.example.memoreels.domain.model.Favorite;
+import com.example.memoreels.domain.model.Video;
+import com.example.memoreels.domain.repository.VideoRepository;
+import kotlinx.coroutines.flow.Flow;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@javax.inject.Singleton()
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000D\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000b\n\u0002\b\u0006\b\u0007\u0018\u00002\u00020\u0001B\u0017\b\u0007\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0005\u00a2\u0006\u0002\u0010\u0006J\u0014\u0010\u0007\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\n0\t0\bH\u0016J\u0018\u0010\u000b\u001a\u0004\u0018\u00010\f2\u0006\u0010\r\u001a\u00020\u000eH\u0096@\u00a2\u0006\u0002\u0010\u000fJ\"\u0010\u0010\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\f0\u00110\b2\f\u0010\u0012\u001a\b\u0012\u0004\u0012\u00020\u000e0\tH\u0016J\u0016\u0010\u0013\u001a\u00020\u00142\u0006\u0010\r\u001a\u00020\u000eH\u0096@\u00a2\u0006\u0002\u0010\u000fJ*\u0010\u0015\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\f0\u00110\b2\u0006\u0010\u0016\u001a\u00020\u000e2\f\u0010\u0012\u001a\b\u0012\u0004\u0012\u00020\u000e0\tH\u0016J\u0016\u0010\u0017\u001a\u00020\u00142\u0006\u0010\u0018\u001a\u00020\fH\u0096@\u00a2\u0006\u0002\u0010\u0019R\u000e\u0010\u0004\u001a\u00020\u0005X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u001a"}, d2 = {"Lcom/example/memoreels/data/repository/VideoRepositoryImpl;", "Lcom/example/memoreels/domain/repository/VideoRepository;", "videoPagingSourceFactory", "Lcom/example/memoreels/data/datasource/VideoPagingSourceFactory;", "favoritesDao", "Lcom/example/memoreels/data/local/FavoritesDao;", "(Lcom/example/memoreels/data/datasource/VideoPagingSourceFactory;Lcom/example/memoreels/data/local/FavoritesDao;)V", "getFavorites", "Lkotlinx/coroutines/flow/Flow;", "", "Lcom/example/memoreels/domain/model/Favorite;", "getVideoByUri", "Lcom/example/memoreels/domain/model/Video;", "uri", "", "(Ljava/lang/String;Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "getVideos", "Landroidx/paging/PagingData;", "exclusions", "isFavorite", "", "searchVideos", "query", "toggleFavorite", "video", "(Lcom/example/memoreels/domain/model/Video;Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "app_debug"})
+public final class VideoRepositoryImpl implements com.example.memoreels.domain.repository.VideoRepository {
+    @org.jetbrains.annotations.NotNull()
+    private final com.example.memoreels.data.datasource.VideoPagingSourceFactory videoPagingSourceFactory = null;
+    @org.jetbrains.annotations.NotNull()
+    private final com.example.memoreels.data.local.FavoritesDao favoritesDao = null;
+    
+    @javax.inject.Inject()
+    public VideoRepositoryImpl(@org.jetbrains.annotations.NotNull()
+    com.example.memoreels.data.datasource.VideoPagingSourceFactory videoPagingSourceFactory, @org.jetbrains.annotations.NotNull()
+    com.example.memoreels.data.local.FavoritesDao favoritesDao) {
+        super();
+    }
+    
+    @java.lang.Override()
+    @org.jetbrains.annotations.NotNull()
+    public kotlinx.coroutines.flow.Flow<androidx.paging.PagingData<com.example.memoreels.domain.model.Video>> getVideos(@org.jetbrains.annotations.NotNull()
+    java.util.List<java.lang.String> exclusions) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    @org.jetbrains.annotations.NotNull()
+    public kotlinx.coroutines.flow.Flow<androidx.paging.PagingData<com.example.memoreels.domain.model.Video>> searchVideos(@org.jetbrains.annotations.NotNull()
+    java.lang.String query, @org.jetbrains.annotations.NotNull()
+    java.util.List<java.lang.String> exclusions) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    @org.jetbrains.annotations.Nullable()
+    public java.lang.Object getVideoByUri(@org.jetbrains.annotations.NotNull()
+    java.lang.String uri, @org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super com.example.memoreels.domain.model.Video> $completion) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    @org.jetbrains.annotations.NotNull()
+    public kotlinx.coroutines.flow.Flow<java.util.List<com.example.memoreels.domain.model.Favorite>> getFavorites() {
+        return null;
+    }
+    
+    @java.lang.Override()
+    @org.jetbrains.annotations.Nullable()
+    public java.lang.Object toggleFavorite(@org.jetbrains.annotations.NotNull()
+    com.example.memoreels.domain.model.Video video, @org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super java.lang.Boolean> $completion) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    @org.jetbrains.annotations.Nullable()
+    public java.lang.Object isFavorite(@org.jetbrains.annotations.NotNull()
+    java.lang.String uri, @org.jetbrains.annotations.NotNull()
+    kotlin.coroutines.Continuation<? super java.lang.Boolean> $completion) {
+        return null;
+    }
+}
